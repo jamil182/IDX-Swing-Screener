8d658406ba44816a77e070302c35a39d34205be8
jamil182 <jamilrivan@Gmail.com>
2026-01-22 18:35:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -169,7 +169,7 @@ def get_data():
 
 # Tampilan Header
 st.title("📈 IDX Live Stock Screener")
-st.write(f"Update Terakhir: {datetime.now().strftime('%H:%M:%S')} WIB")
+st.write(f"**Last updated:** {waktu_sekarang} WIB (Auto-refresh every 15s)")
 
 # Load Data
 df = get_data()
